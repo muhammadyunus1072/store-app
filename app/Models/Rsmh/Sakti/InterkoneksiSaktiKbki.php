7edac44557da802071a1b35cb718bfbab1aaa6ec
Bhagaskara <bhagaskaraliancer@gmail.com>
2024-12-16 11:52:22 +0700

    Interkoneksi Sakti

--- app/Models/Rsmh/Sakti/InterkoneksiSaktiKbki.php
+++ app/Models/Rsmh/Sakti/InterkoneksiSaktiKbki.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Models\Rsmh\InterkoneksiSakti;
+namespace App\Models\Rsmh\Sakti;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class InterkoneksiSaktiHeader extends Model
+class InterkoneksiSaktiKbki extends Model
 {
     use HasFactory;
 }
